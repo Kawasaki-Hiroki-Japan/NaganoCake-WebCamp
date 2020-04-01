be808691167ed48356173441e5676b506e78f62e
kawasaki-hiroki-infratop <kawasaki.hiroki.infratop@gmail.com>
2020-04-01 22:19:57 +0900

    「ログインユーザのアカウント管理機能」完成

--- app/controllers/public/end_users_controller.rb
+++ app/controllers/public/end_users_controller.rb
@@ -2,4 +2,27 @@ class Public::EndUsersController < ApplicationController
   before_action :authenticate_end_user!
   def show
   end
+  def edit
+  end
+  def update
+    end_user = current_end_user
+    if end_user.update(end_user_params)
+      redirect_to end_user_path
+    else
+      render :edit
+    end
+  end
+  def withdraw
+  end
+  def destroy
+    end_user = current_end_user
+    end_user.update(status: :false)
+    end_user.destroy
+    redirect_to new_end_user_session_path
+  end
+
+  private
+  def end_user_params
+    params.require(:end_user).permit(:first_name, :last_name, :first_kana, :last_kana, :postal_code, :address, :phone_number, :email)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,20 +1,31 @@
 Rails.application.routes.draw do
 
-  devise_for :end_users, controllers: {
-    sessions:      'end_users/sessions',
-    passwords:     'end_users/passwords',
-    registrations: 'end_users/registrations'
-  }
-  scope module: :public do
-    get 'end_users/:id' => 'end_users#show', as: 'end_user'
+  devise_for :end_users, skip: :all
+  devise_scope :end_user do
+    get 'end_users/sign_in' => 'end_users/sessions#new', as: 'new_end_user_session'
+    post 'end_users/sign_in' => 'end_users/sessions#create', as: 'end_user_session'
+    delete 'end_users/sign_out' => 'end_users/sessions#destroy', as: 'destroy_end_user_session'
+    get 'end_users/sign_up' => 'end_users/registrations#new', as: 'new_end_user_registration'
+    post 'end_users' => 'end_users/registrations#create', as: 'end_user_registration'
   end
 
-  devise_for :admin, controllers: {
-    sessions:      'admins/sessions',
-    passwords:     'admins/passwords',
-    registrations: 'admins/registrations'
-  }
+  scope module: :public do
+    get 'end_users' => 'end_users#show', as: 'end_user'
+    get 'end_users/edit' => 'end_users#edit', as: 'edit_end_user'
+    patch 'end_users' => 'end_users#update', as: 'update_end_user'
+    get 'end_users/withdraw' => 'end_users#withdraw', as: 'withdraw'
+    delete 'end_users' => 'end_users#destroy', as: 'destroy_end_user'
+  end
+
+  devise_for :admins, skip: :all
+  devise_scope :admin do
+    get 'admin/sign_in' => 'admins/sessions#new', as: 'new_admin_session'
+    post 'admin/sign_in' => 'admins/sessions#create', as: 'admin_session'
+    delete 'admin/sign_out' => 'admins/sessions#destroy', as: 'destroy_admin_session'
+  end
+
   namespace :admin do
     get 'items' => 'items#index', as: 'items'
+    get 'end_users' => 'end_users#index', as: 'end_users'
   end
 end
